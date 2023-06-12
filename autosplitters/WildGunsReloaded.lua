1af9dfefddfc64dc54c9528b4a55e9afde0f25a0
Wins1ey <34382191+Wins1ey@users.noreply.github.com>
2023-06-12 18:12:21 +0100

    Added WildGunsReloaded.lua

---
+++ autosplitters/WildGunsReloaded.lua
@@ -0,0 +1,23 @@
+processID('WildGunsReloaded.exe')
+
+local baseAddress = 0x00400000
+
+local prevPlayerSelected
+local prevMultiPlayerSelected
+
+while true do
+    local playerSelected = readAddress(8, 0x01093FAC, baseAddress, 0x50, 0x4, 0x8, 0x8, 0x34);
+    local multiPlayerSelected = readAddress(8, 0x0100CCA8, baseAddress, 0x30, 0x168, 0x16C, 0xC, 0x38, 0x2C);
+    local goTitle = readAddress(8, 0x01093FAC, baseAddress, 0x40, 0x0, 0x8, 0x8, 0x6C)
+
+    --if playerSelected ~= prevPlayerSelected or multiPlayerSelected ~= prevMultiPlayerSelected then
+    print(playerSelected .. ' ' .. multiPlayerSelected)
+    --end
+
+    if playerSelected == 3 or multiPlayerSelected == 3 then
+        sendCommand('starttimer\r\n')
+    end
+
+    prevPlayerSelected = playerSelected
+    prevMultiPlayerSelected = multiPlayerSelected
+end
