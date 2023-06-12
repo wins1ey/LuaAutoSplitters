30053eaf872053d6555a76d553e257dfc1849685
Wins1ey <34382191+Wins1ey@users.noreply.github.com>
2023-06-12 20:24:42 +0100

    Reordered basAddress and windows addresses

--- autosplitters/WildGunsReloaded.lua
+++ autosplitters/WildGunsReloaded.lua
@@ -6,9 +6,9 @@ local prevPlayerSelected
 local prevMultiPlayerSelected
 
 while true do
-    local playerSelected = readAddress(8, 0x01093FAC, baseAddress, 0x50, 0x4, 0x8, 0x8, 0x34);
-    local multiPlayerSelected = readAddress(8, 0x0100CCA8, baseAddress, 0x30, 0x168, 0x16C, 0xC, 0x38, 0x2C);
-    local goTitle = readAddress(8, 0x01093FAC, baseAddress, 0x40, 0x0, 0x8, 0x8, 0x6C)
+    local playerSelected = readAddress(8, baseAddress, 0x01093FAC, 0x50, 0x4, 0x8, 0x8, 0x34);
+    local multiPlayerSelected = readAddress(8, baseAddress, 0x0100CCA8, 0x30, 0x168, 0x16C, 0xC, 0x38, 0x2C);
+    local goTitle = readAddress(8, baseAddress, 0x01093FAC, 0x40, 0x0, 0x8, 0x8, 0x6C)
 
     --if playerSelected ~= prevPlayerSelected or multiPlayerSelected ~= prevMultiPlayerSelected then
     print(playerSelected .. ' ' .. multiPlayerSelected)
