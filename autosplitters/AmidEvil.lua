9d4197525818c2bb84dd63ce5d4db921499db42d
Wins1ey <34382191+Wins1ey@users.noreply.github.com>
2023-06-14 19:24:25 +0100

    Updated autosplitters to work with new update

--- autosplitters/AmidEvil.lua
+++ autosplitters/AmidEvil.lua
@@ -1,6 +1,6 @@
-processID('AmidEvil-Win64-Shipping.exe')
+process('AmidEvil-Win64-Shipping.exe')
 
-local baseAddress = 0x140000000
+sendCommand('initgametime')
 
 local prevStart
 local prevLoading
@@ -23,25 +23,25 @@ elseif mode == 2 then
 end
 
 while true do
-    local start = readAddress(8, baseAddress, 0x2BAFFD0)
-    local loading = readAddress(8, baseAddress, 0x2E76B0C)
-    local menuStage = readAddress(8, baseAddress, 0x2F75F14)
-    local paused = readAddress(8, baseAddress, 0x2B95A68)
+    local start = readAddress("byte", 0x2BAFFD0)
+    local loading = readAddress("byte", 0x2E76B0C)
+    local menuStage = readAddress("byte", 0x2F75F14)
+    local paused = readAddress("byte", 0x2B95A68)
 
     if mode == 2 and (loading == 0 and prevLoading == 1) then
-        sendCommand('starttimer\r\n')
+        sendCommand('starttimer')
     elseif start == 0 and prevStart == 2 then
-        sendCommand('starttimer\r\n')
+        sendCommand('starttimer')
     end
 
     if (loading == 1 and prevLoading == 0) or (menuStage == 3 and paused == 4) and (prevMenuStage ~= 3 or prevPaused ~= 4) then
-        sendCommand('pausegametime\r\n')
+        sendCommand('pausegametime')
     elseif (loading ~= 1 and prevLoading == 1) and (menuStage ~= 3 or paused ~= 4) then
-        sendCommand('unpausegametime\r\n')
+        sendCommand('unpausegametime')
     end
 
     if (menuStage == 3 and prevMenuStage == 2) and paused ~= 28 and paused ~= 3 then
-        sendCommand('split\r\n')
+        sendCommand('split')
     end
 
     prevStart = start
